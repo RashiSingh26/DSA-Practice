a0eb8aea0749d9487d4a039a906e145aa31c75ea
RashiSingh26 <singhrashi6204@gmail.com>
2026-03-09 22:47:49 +0530

    Update 0724-find-pivot-index.java

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,24 +1,27 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int totalSum = 0;
-        
+        int n=nums.length;
+        int prefix[]=new int[n];
+        int suff[]=new int[n];
 
-        for (int num : nums) {
-            totalSum += num;
+
+    
+prefix[0]=0;
+        for(int i=1;i<n;i++){
+            prefix[i]=prefix[i-1]+nums[i-1];
+
         }
-        
-        int leftSum = 0;
-     
-        for (int i = 0; i < nums.length; i++) {
-            
-          
-            if (leftSum == totalSum - leftSum - nums[i]) {
+
+        suff[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            suff[i]=suff[i+1]+nums[i+1];
+        }
+        for(int i=0;i<n;i++){
+            if(prefix[i]==suff[i]){
                 return i;
             }
-            
-            leftSum += nums[i];
+
         }
-        
-        return -1;
+return -1;
     }
 }
