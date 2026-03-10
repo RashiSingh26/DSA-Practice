9a8eb054eb6d21596a3be79091de6ad1a1836d77
RashiSingh26 <singhrashi6204@gmail.com>
2026-03-10 21:43:04 +0530

    Create upperbound_binarysearch.java

---
+++ upperbound_binarysearch.java
@@ -0,0 +1,21 @@
+// User function Template for Java
+class Solution {
+    public int findCeil(int[] arr, int x) {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int res=-1;
+        while(low<=high){
+            int guess=(low+high)/2;
+            if(arr[guess]<x){
+                low=guess+1;
+            }
+            else{
+                res=guess;
+                high=guess-1;
+            }
+        }
+        return res;
+        
+    }
+}
