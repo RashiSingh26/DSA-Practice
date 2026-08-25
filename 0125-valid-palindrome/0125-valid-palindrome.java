c0903d72b616870ba56b9e0269016390144a1921
Rashi singh <singhrashi6204@gmail.com>
2026-08-26 02:32:44 +0530

    Time: 14 ms (28.47%), Space: 46.1 MB (33.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       
-
         s=s.toLowerCase().replaceAll("[^a-z0-9]","");
         int n=s.length();
         int left=0;
         int right=n-1;
-
         while(left<right){
-            if(s.charAt(left) != s.charAt(right)){
-                return false;
-            }
-                left++;
-                right--;
-            
+            if(s.charAt(left)!=s.charAt(right))
+            return false;
+            left++;
+            right--;
         }
         return true;
-    }
+
+
+
+            }
+
 }
