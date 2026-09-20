725615108470a58f0d3c387ec7bd4fb500861cdb
Rashi singh <singhrashi6204@gmail.com>
2026-09-20 22:24:27 +0530

    Time: 1 ms (56.04%), Space: 47.5 MB (89.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,6 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-
         int left=0;
         int right=n-1;
 
@@ -13,23 +12,22 @@ class Solution {
             if(height[left]<height[right]){
                 if(height[left]>leftmax){
                     leftmax=height[left];
+
                 }
                 else{
                     water+=leftmax-height[left];
                     left++;
                 }
             }
-                else{
-                    if(height[right]>rightmax){
-                        rightmax=height[right];
-
-                    }
-                    else{
-                        water+=rightmax-height[right];
-                        right--;
-                    }
+            else{
+                if(height[right]>rightmax){
+                    rightmax=height[right];
                 }
-            
+                else{
+                    water+=rightmax-height[right];
+                    right--;
+                }
+            }
         }
         return water;
     }
